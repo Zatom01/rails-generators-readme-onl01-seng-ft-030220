2b0834d85ab277246a553132cd1bfdfeadd2b93a
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-14 21:40:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200514213554) do
+ActiveRecord::Schema.define(version: 20200514213938) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,10 +21,9 @@ ActiveRecord::Schema.define(version: 20200514213554) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
   end
 
 end
